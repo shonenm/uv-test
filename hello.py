4bd96fbd6de1e91025456b1ca07401d035d395fe
SyounenM <matsukota42274@g.ecc.u-tokuo.ac.jp>
2024-12-25 22:36:09 +0900

    feat: before ruff format

--- hello.py
+++ hello.py
@@ -1,5 +1,19 @@
+import os, sys, time
+
 def main():
     print("Hello from uv-test!")
+    current_dir = os.getcwd()
+    if os.path.exists(current_dir):
+        print("Current directory: " + current_dir)
+    else:
+        print("Current directory not found")
+        
+    for i in range(5):
+        time.sleep(1)
+        if not i == 0:
+            print(f"{i} seconds elapsed")
+        if i == 3:
+            break
 
 
 if __name__ == "__main__":
